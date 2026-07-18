47afbef2fdd7b910fb129a70d9ad137542b2774b
Indrachapa Jayasinghe <indrachapaholu19@gmail.com>
2026-07-18 15:27:45 +0530

    Sign out button redirects to the home page after signing out.

--- src/components/layout/sign-out-button.tsx
+++ src/components/layout/sign-out-button.tsx
@@ -24,7 +24,7 @@ export function SignOutButton({
     const supabase = createClient();
     await supabase.auth.signOut();
     router.refresh();
-    router.push("/login");
+    router.push("/");
   }
 
   return (
